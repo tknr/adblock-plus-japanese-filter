a82e76709f57b9581320317e2038bc37ae76ff05
k2japan <k2japan@gmail.com>
2012-01-31 15:49:27 +0900

    Minor adjustment

---
+++ build/prepare2commitAll.bat
@@ -0,0 +1,13 @@
+@rem ######## Update Last-modified date string ########
+@perl ../pl/updateDateString.pl ../abp_jp.txt
+@perl ../pl/updateDateString.pl ../abp_jp_paranoid.txt
+
+@rem ######## Add Checksum ########
+@perl ../pl/addChecksum.pl ../abp_jp.txt
+@perl ../pl/addChecksum.pl ../abp_jp_paranoid.txt
+
+@rem ######## Validate Checksum ########
+@perl ../pl/validateChecksum.pl ../abp_jp.txt
+@perl ../pl/validateChecksum.pl ../abp_jp_paranoid.txt
+
+@rem @pause
---
+++ pl/updateDateString.pl
@@ -0,0 +1,58 @@
+﻿#!/usr/bin/perl
+
+###################################################################################
+# This script updates date string like "! Last modified: 2010-04-06 14:03:42 JST" #
+#                                                                                 #
+# To add or update date string, run the script like this:                         #
+#                                                                                 #
+#   perl updateDateString.pl subscription.txt                                     #
+#                                                                                 #
+# Note: your subscription file should be saved in UTF-8 encoding, otherwise       #
+#       the generated checksum might be incorrect.                                #
+#                                                                                 #
+###################################################################################
+
+use strict;
+use warnings;
+use Time::Piece qw(localtime);
+
+die "Usage: $^X $0 subscription.txt\n" unless @ARGV;
+
+my $file = $ARGV[0];
+my $data = readFile($file);
+
+my $time    = localtime();
+# my $strTime = $time->strftime("%d %b %Y %H:%M JST");
+my $strTime = $time->strftime("%F %T JST");
+
+die "[ERR] Failed to Generate Date String!" unless $strTime;
+
+# Replace already existing "! Last modified: DATE TIME JST"
+$data =~ s/^.*!\s*Last\s+modified[\s\-:]+([\w\+\/=]+).*$/! Last modified: $strTime/gmi;
+
+writeFile($file, $data);
+
+######## Sub START ########
+sub readFile
+{
+  my $file = shift;
+
+  open(local *FILE, "<", $file) || die "Could not read file '$file'";
+  binmode(FILE);
+  local $/;
+  my $result = <FILE>;
+  close(FILE);
+
+  return $result;
+}
+
+sub writeFile
+{
+  my ($file, $contents) = @_;
+
+  open(local *FILE, ">", $file) || die "Could not write file '$file'";
+  binmode(FILE);
+  print FILE $contents;
+  close(FILE);
+}
+######## Sub END ########
