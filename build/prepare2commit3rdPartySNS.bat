1be70f50ad3b393ca54c2ed7090e048ecec5c304
k2japan <k2japan@gmail.com>
2012-11-22 22:59:00 +0900

    Added experimental 3rd party filters

--- build/prepare2commit3rdPartySNS.bat
+++ build/prepare2commit3rdPartySNS.bat
@@ -1,16 +1,10 @@
 @rem ######## Update Last-modified date string ########
-@perl ../pl/updateDateString.pl ../abp_jp.txt
-@perl ../pl/updateDateString.pl ../abp_jp_paranoid.txt
 @perl ../pl/updateDateString.pl ../abp_jp_3rd_party_SNS.txt
 
 @rem ######## Add Checksum ########
-@perl ../pl/addChecksum.pl ../abp_jp.txt
-@perl ../pl/addChecksum.pl ../abp_jp_paranoid.txt
 @perl ../pl/addChecksum.pl ../abp_jp_3rd_party_SNS.txt
 
 @rem ######## Validate Checksum ########
-@perl ../pl/validateChecksum.pl ../abp_jp.txt
-@perl ../pl/validateChecksum.pl ../abp_jp_paranoid.txt
 @perl ../pl/validateChecksum.pl ../abp_jp_3rd_party_SNS.txt
 
 @rem @pause
